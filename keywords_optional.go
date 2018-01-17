13e8f5a9a6c812ba6b6ae3f97a523d653853518c
Thomas Osterbind <tom@qri.io>
2018-01-17 15:12:24 -0500

    chore: added skeleton for keywords_optional

---
+++ keywords_optional.go
@@ -0,0 +1,42 @@
+package jsonschema
+
+// import (
+// 	"encoding/json"
+// 	"fmt"
+// )
+
+// BigNum ...
+// TODO: explain, set to correct type
+type BigNum int
+
+// Validate implements the Validator interface for BigNum
+func (b BigNum) Validate(data interface{}) error {
+	//TODO: implement
+}
+
+// Content ...
+// TODO: explain, set to correct type
+type Content int
+
+// Validate implements the Validator interface for Content
+func (c Content) Validate(data interface{}) error {
+	//TODO: implement
+}
+
+// EcmascriptRegex ...
+// TODO: explain, set to correct type
+type EcmascriptRegex int
+
+// Validate implements the Validator interface for EcmascriptRegex
+func (e EcmascriptRegex) Validate(data interface{}) error {
+	//TODO: implement
+}
+
+// ZeroTerminatedFloats ...
+// TODO: explain, set to correct type
+type ZeroTerminatedFloats int
+
+// Validate implements the Validator interface for EcmascriptRegex
+func (z ZeroTerminatedFloats) Validate(data interface{}) error {
+	//TODO: implement
+}
